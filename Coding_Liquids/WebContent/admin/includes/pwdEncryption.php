98dd06c6f97c6529c4e0d674606fcf91f43e9271
soumyadip007 <soumyadip.note@gmail.com>
2019-02-08 13:51:53 +0530

    Version 0.3

--- Coding_Liquids/WebContent/admin/includes/pwdEncryption.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*Password encryption code*/
-$hashFormat = "$2a$07$";  //hashFormat for BLOWFISH algorithm
-$salt = "iAuUssgmehjrazitringF$";  //22 characters long salt string
-$hashF_and_salt = $hashFormat.$salt;
-
-?>
