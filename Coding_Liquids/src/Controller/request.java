bf6da928507b7fdd8220c83cde45d01698302e63
soumyadip007 <soumyadip.note@gmail.com>
2019-03-25 12:13:27 +0530

    Request MVC Update

--- Coding_Liquids/src/Controller/request.java
+++ Coding_Liquids/src/Controller/request.java
@@ -12,8 +12,7 @@ public class request extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
 	}
 
 }
